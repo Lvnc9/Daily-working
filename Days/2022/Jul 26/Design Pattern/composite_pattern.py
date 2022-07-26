29ff7eee5fcfab69e3cfc19b71c8c5924179ff84
lvnc9 <sam.mohammadi2154@gmail.com>
2022-07-26 13:15:14 +0430

    composite-updated

--- Days/2022/Jul 26/Design Pattern/composite_pattern.py
+++ Days/2022/Jul 26/Design Pattern/composite_pattern.py
@@ -54,4 +54,25 @@ class AbstractCompositeItem(AbstractItem):
     def remove(self, item):
         self.children.remove(item)
     
+    def __iter__(self):
+        return iter(self.children)
+    
+class CompositeItem(AbstractCompositeItem):
+
+    def __init__(self, name, *items):
+        super().__init__(*items)
+        self.name = name
+    
+    def composite(self):
+        return True
+
+    @property
+    def price(self):
+        return sum(item.price for item in self)
+    
+    def print(self, indent='', file=stdout):
+        print("{}${:.2f}{}".format(indent, self.price, self.name), file=file)
+        for child in self:
+            child.print(indent + "      ")
+
 # End
