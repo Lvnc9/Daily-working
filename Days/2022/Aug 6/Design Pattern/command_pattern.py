7f2361d8e3e97723b6afc9216237bb9ef07c8f45
lvnc9 <sam.mohammadi2154@gmail.com>
2022-08-06 16:08:30 +0430

    command pattern

--- Days/2022/Aug 6/Design Pattern/command_pattern.py
+++ Days/2022/Aug 6/Design Pattern/command_pattern.py
@@ -7,9 +7,9 @@ import code
 
 class Grid:
     def __init__(self, width, height):
-        self.__cells = [['white' for _ in (height)] 
+        self.__cells = [['white' for _ in (height)]
                         for _ in (width)]
-    
+
     def cell(self, x, y, color=None):
         if color is None:
             return self.__cells[x][y]
@@ -18,10 +18,21 @@ class Grid:
     @property
     def rows(self):
         return len(self.__cells[0])
-    
+
     @property
     def columns(self):
         return len(self.__cells)
 
 
+class UndoGrid(Grid):
+
+    def create_cell_command(self, x, y, color):
+        def undo():
+            self.cell(x, y, undo.color)
+
+        def do():
+            undo.color = self.cell(x, y)
+            self.cell(x, y, color)
+        return Command.command(do, undo, "Cell")
+        
 # End
