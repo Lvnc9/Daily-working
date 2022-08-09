22b79188799f8589fb3e49f089e7cc4851d75269
lvnc9 <sam.mohammadi2154@gmail.com>
2022-08-09 15:33:12 +0430

    interpreterPattern

--- Days/2022/Aug 8/Design Pattern/interpreterPattern.py
+++ Days/2022/Aug 8/Design Pattern/interpreterPattern.py
@@ -2,8 +2,47 @@
 # Start
 # IDk yet but that can be acceptable
 # Modules
+import math
+import collections
+import types
+import sys
 
 
+def global_context():
+    GlobalContext = globals().copy()
+    for name in dir(math):
+        global_context[name] = getattr(math, name)
+    return global_context
+
+def calculate(expression, globalContaxt, localContaxt, result, current):
+    try:
+        result = eval(expression, globalContaxt, localContaxt)
+        update(localContaxt, result, current)
+        print(", ".join([f"{value}:{variable}"
+                        for value, variable in localContaxt.items()]))
+        print(f"ANS={result}")
+    except Exception as err:
+        print(err)
+
+def update(localContaxt, result, current):
+    localContaxt[current.letter] = result
+    current.letter = chr(ord(current.letter) + 1)
+    if current.etter > "Z" : # We Only support 26 variables
+        current.letter = "A" 
 
 
+def main():
+    quit = "Ctrl+Z,Enter" if sys.platform.startswith("win") else "Ctrl+D"
+    prompt = "Enter an expression ({} to quit): ".format(quit)
+    current = types.SimpleNamespace(letter="A")
+    globalContext = global_context()
+    localContext = collections.OrderedDict()
+    while True:
+        try:
+            expression = input(prompt)
+            if expression:
+                calculate(expression, globalContext, localContext, current)
+        except EOFError:
+            print()
+            break
 # End
