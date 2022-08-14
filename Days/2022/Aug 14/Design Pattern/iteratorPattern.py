0d252340e6693a17909c0ec0c28ec46f5579147e
lvnc9 <sam.mohammadi2154@gmail.com>
2022-08-14 13:04:44 +0430

    iterator Pattern

--- Days/2022/Aug 14/Design Pattern/iteratorPattern.py
+++ Days/2022/Aug 14/Design Pattern/iteratorPattern.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Start
-# Using iterator pattern for accecing data and files without exposing 
+# Using iterator pattern for accecing data and files without exposing
 # to its collection or aggregate
 # Module
 
@@ -12,9 +12,35 @@ class AtoZ:
             return chr(index + ord("A"))
         raise IndexError()
 
+
 for item in AtoZ():
     print(item, end="")
 print()
 
+
+# Implementition for Iter function
+class Presidents:
+    """ Iterating over the presidents of US
+    Simple usage just Iterate over the object """
+
+    __name = ("George Washington",
+              "John Adams",
+              "Thomas Jefferson",
+              "Bill Clinton", 
+              "George W. Bush", 
+              "Barack Obama")
+
+    def __init__(self, first=None):
+        self.index = (-1 if first is None else
+                      Presidents.__name.index(first) - 1)
+    
+    def __call__(self):
+        self.index += 1
+        if self.index < len(Presidents.__name):
+            return Presidents.__name[self.index]
+        raise StopIteration()
+
+for pres in iter(Presidents('George Washington'), "Georg W. Bush"):
+    print(pres, end="")
 
 # End
