f2e21243f3229c9bd27fce80ae761ff72653ae9f
lvnc9 <sam.mohammadi2154@gmail.com>
2022-08-16 14:26:12 +0430

    mediatorPattern

--- Days/2022/Aug 15/Desing Pattern/mediatorPattern.py
+++ Days/2022/Aug 15/Desing Pattern/mediatorPattern.py
@@ -8,18 +8,18 @@ from collections import defaultdict
 class Form:
 
     def __init__(self):
-        self.create_widget = create_widget()
-        self.create_mediator = create_mediator()
+        self.create_widget()
+        self.create_mediator()
     
     def create_widget(self):
         self.name_text = Text()
         self.email_text = Text()
         self.ok_button = Button("OK")
-        self.cancel_button = Botton("CANCEL")
+        self.cancel_button = Button("CANCEL")
     
-    def create_medator(self):
-        self.mediator = Mediator((self.name_text, update_ui),
-                                (self.email_text, update_ui),
+    def create_mediator(self):
+        self.mediator = Mediator((self.name_text, self.update_ui),
+                                (self.email_text, self.update_ui),
                                 (self.ok_button, self.clicked),
                                 (self.cancel_button, self.clicked))
         self.update_ui()
@@ -35,14 +35,50 @@ class Form:
             print("Cancel")
 
     
-    class Mediator:
-        
-        def __init__(self, widgetCallablePairs):
-            self.callable_for_widgets = defaultdict(list)
-            for widget, caller in widgetCallablePairs:
-                self.callablesForWidget[widget].append(caller)
-                widget.mediator = self
+class Mediator:
+    
+    def __init__(self, widgetCallablePairs):
+        self.callable_for_widgets = defaultdict(list)
+        for widget, caller in widgetCallablePairs:
+            self.callablesForWidget[widget].append(caller)
+            widget.mediator = self
+
+    def on_change(self, widget):
+        callables = self.callable_for_widgets.get(widget)
+        if callables is not None:
+            for caller in callables:
+                caller(widget)
+        else:
+            raise AttributeError(F"no on_change() method registered for {widget}")
+
+class Mediated:
+    
+    def __init__(self):
+        self.mediator = None
+    
+    def on_change(self):
+        if self.mediator is not None:
+            self.mediator.on_change(self)
+
+class Button(Mediated):
+
+    def __init__(self, text=""):
+        super().__init__()
+        self.enabled = True
+        self.text = text
+
+    def click(self):
+        if self.enabled:
+            self.on_change()
 
 
+class Text(Mediated):
 
+    def __init__(self, text=""):
+        super().__init__()
+        self.__text = text
+    
+    @property
+    def text(self):
+        return self.__text
 # End
