3266d4be5b326b1aa327bc8457c033d0893e7ad6
lvnc9 <sam.mohammadi2154@gmail.com>
2022-08-17 12:28:29 +0430

    coroutine based Mediator

---
+++ Days/2022/Aug 17/coroutineBasedMediator.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# Start
+# Making an Mediator Object usig coroutines and chaincalls
+# Moduels:
+from functools import wraps
+
+# For convertingg Generators inside form to coroutines using decorators
+def coroutine(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        generator = func(*args, **kwargs)
+        next(generator)
+        return generator
+    return wrapper
+
+
+class Form:
+
+    def create_mediator(self):
+        self.mediator = self._update_ui_mediator(self.clicked_mediator())
+        for widget in (self.nameText, self.emailText,
+                       self.okButton, self.cancelBotton):
+            widget.mediator = self.mediator
+        self.mediator.send(None)
+
+    @coroutine
+    def _update_ui_mediator(self, successor=None):
+        # one of the good usages of this update_ui is making the code shorter
+        # and the written is less complex and more usefull
+        while True:
+            widget = yield
+            self.okButton.enabled = (bool(self.nameText) and
+                                     bool(self.email_text))
+            if successor is not None:
+                successor.send(widget)
+
+    @coroutine
+    def clicked_mediator(self, successor=None):
+        while True:
+            widget = yield
+            if widget == self.okButton:
+                print("OK")
+            elif widget == self.cancelBotton:
+                print("Cancel")
+            elif successor is not None:
+                successor.send(widget)
+
+
+class Mediated:
+    
+    def __init__(self):
+        self.mediator = None
+    
+    def on_change(self):
+        if self.mediator is not None:
+            self.mediator.senf(self)
+
+# End
