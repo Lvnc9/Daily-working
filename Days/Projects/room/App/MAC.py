0840f34a72ad05fb39586a1248e9bb59f7b03bae
lvnc9 <sam.mohammadi2154@gmail.com>
2022-09-07 12:04:10 +0430

    logMac.py

---
+++ Days/Projects/room/App/MAC.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# Start
+# MAC Module
+# Modules
+import logMac
+import collections 
+
+
+class Mac:
+    """ USAGE  will get added as soon as possible """
+
+    def __init__(self):
+        self.logger = logMac.LogWriter()
+    
+        
+    class Nature:
+        pass
+
+
+    class Physicality:
+        pass
+
+    
+    class Living():
+        pass
+
+    
+
+
+# End
