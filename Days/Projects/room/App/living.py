0f66b3f6905f9df516852bd4c3d7fa403409989d
lvnc9 <sam.mohammadi2154@gmail.com>
2022-09-11 13:28:04 +0430

    cat, dog, fish, bird, changed

--- Days/Projects/room/App/living.py
+++ Days/Projects/room/App/living.py
@@ -3,52 +3,45 @@
 
 class Living:
 
-    @classmethod
-    def animal_behaviors(cls, *pets):
-        for pet in pets:
-            n_pet = Pet.animal_list(pet)
-            if n_pet == "cat":
-                attrs = {
-                    "sleeping" : True,
-                    "friendly" : True,
-                    "jumpping" : True,
-                    "sound" : True,
-                    "mew" : True,
-                    "snoring" : True,
-                    "moving" : True,
-                }
-                return attrs
-            elif pet == "dog":
-                attrs = {
-                    "sleeping" : True,
-                    "friendly" : True,
-                    "sound" : True,
-                    "jumpping" : True,
-                    "barking" : True,
-                    "moving" : True
-                }
-                return attrs
-            elif pet == "bird":
-                attrs = {
-                    "sleeping" : True,
-                    "friendly" : True,
-                    "sound" : True,
-                    "flying" : True,
-                    "talking" : True,
-                }
-                return attrs
-            elif pet == "fish":
-                attrs = {
-                    "sleeping" : True,
-                    "friendly" : False,
-                    "jumpping" : False,
-                    "sound" : False,
-                    "swiming" : True,
-                    "bobbling" : True
-                }
-                return attrs
-            else:
-                raise ValueError("")
+    def __init__(self):
+        self.cats = {
+            "sleeping" : True,
+            "friendly" : True,
+            "jumpping" : True,
+            "sound" : True,
+            "mew" : True,
+            "snoring" : True,
+            "moving" : True,
+            }
+    
+        self.dogs = {
+            "sleeping" : True,
+            "friendly" : True,
+            "sound" : True,
+            "jumpping" : True,
+            "barking" : True,
+            "moving" : True
+        }
 
+        self.birds = {
+            "sleeping" : True,
+            "friendly" : True,
+            "sound" : True,
+            "flying" : True,
+            "talking" : True,
+        }
+
+        self.fishes = {
+            "sleeping" : True,
+            "friendly" : False,
+            "jumpping" : False,
+            "sound" : False,
+            "swiming" : True,
+            "bobbling" : True
+        }
+
+    @classmethod
+    def name_adding(self, name):
+        
 
 # End
