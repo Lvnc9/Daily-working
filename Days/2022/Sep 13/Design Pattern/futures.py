1fede0eb1c18caf6e5c9516a7352a306aa4b6241
lvnc9 <sam.mohammadi2154@gmail.com>
2022-09-19 13:03:23 +0430

    using threads FUTURES FILE

--- Days/2022/Sep 13/Design Pattern/futures.py
+++ Days/2022/Sep 13/Design Pattern/futures.py
@@ -5,8 +5,8 @@
 import multiprocessing
 from concurrent.futures import ThreadPoolExecutor
 import os
+import tempfile
 import webbrowser
-
 
 
 print(os.path.join(os.path.dirname(__file__), "whatsnew.dat"))
@@ -29,5 +29,22 @@ def main():
     print()
     if not canceled:
         webbrowser.open(filename)
+
+def process(futures):
+    canceled = False
+    done = 0
+    filename = os.path.join(tempfile.tempdir(), "whatsnew.html")
+    with open(filename, "wt", encoding="utf-8") as file:
+        file.write("<!doctype html>\n")
+        file.write("<html><head><title>What's New</title></head>\n")
+        file.write("<body><h1>What's new</body></h1>\n")
+        canceled, results = wait_for(futures)
+        if not canceled:
+            for result in (result for ok, result in results if ok
+                            and result is not None):
+                done += 1
+                for item in result:
+                    file.write(item)
+
 
 # End
