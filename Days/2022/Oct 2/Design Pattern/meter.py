6be1efd02e0c571dc6e6b6f5b484357d458b4f2e
lvnc9 <sam.mohammadi2154@gmail.com>
2022-10-03 18:24:53 +0330

    get_job

--- Days/2022/Oct 2/Design Pattern/meter.py
+++ Days/2022/Oct 2/Design Pattern/meter.py
@@ -2,6 +2,7 @@
 # Start
 # NETWORKING
 # Modules
+import random
 import xmlrpc
 import collections
 import hashlib
@@ -15,7 +16,6 @@ class Manager:
     SessionId = 0
     UsenameSeassionId = {}
     ReadingForMeter = {}
-    _User = collections.namedtuple("User", "username, sha256")
 
     def login(self, username, password):
         name = name_for_credentials(username, password)
@@ -25,11 +25,27 @@ class Manager:
         sessionId = Manager.SessionId
         Manager.UsernameForSessionId[sessionId] = username
         return sessionId, name
+
+
+    def get_job(self, sessionId):
+        self._username_for_sessionId(sessionId)
+        while True: # Create fake meter
+            kind = random.choise("GE")
+            meter = "{}{}".format(kind, random.randint(40000,
+                    99999 if kind == "G" else  999999))
+            if meter not in Manager.ReadingForMeter:
+                Manager.ReadingForMeter[meter] = None
+                return meter
+
+
+_User = collections.namedtuple("User", "username, sha256")
     
-    def name_for_credentials(username, password):
-        sha = hashlib.sha256()
-        sha.update(password.encode("utf-8"))
-        user = _User(username, sha.hexdigest())
+def name_for_credentials(username, password):
+    sha = hashlib.sha256()
+    sha.update(password.encode("utf-8"))
+    user = _User(username, sha.hexdigest())
+    return _Users.get(user)
+
 
 
 # End
