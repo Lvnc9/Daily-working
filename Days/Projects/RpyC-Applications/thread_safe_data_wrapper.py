174c7122b011a7e5faab60d6e5c7f81a5a306d8c
lvnc9 <sam.mohammadi2154@gmail.com>
2022-10-24 17:50:08 +0330

    thread_safe_data_wrapper_

---
+++ Days/Projects/RpyC-Applications/thread_safe_data_wrapper.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# Start
+# Project of Chapter 6
+# Modules
+import threading
+import concurrent
+import random
+
+
+class Error(Exception): pass
+class Manager:
+
+    SessionId = 0
+    SessionIdLock = threading.Lock() # for serializing access to its static data
+    UsernameForSessionId = ThreadSafeDict()
+    ReadingForMeter = _MeterDict()
+
+    def login(self, username, password):
+        name = name_for_creadentials(username, password)
+        if name is None:
+            raise Error("invalid username or password")
+        with Manager.SessionIdLock:
+            Manager.SessionId += 1
+            sessionId = Manager.SessionId
+        Manager.UsernameForSessionId[sessionId] = username
+        return sessionId, name
+    
+    def get_status(self, sessionId):
+        username = self._username_for_sessionId(sessionId)
+        return Manager.ReadingForMeter.status(sessionId)
+
+    def get_job(self, sessionId):
+        self._username_for_sessionId(sessionId)
+        while True:
+            kind = random.choice("GE")
+            meter = "{}{}".format(kind, random.randint(40000,
+                    99999 if kind == "G" else 999999))
+            if Manager.ReadingForMeter.insert_if_missing(meter):
+                return meter
+
+
+    
+
+
+
+# End
