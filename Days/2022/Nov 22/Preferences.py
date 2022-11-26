c9a9ed1432fe04b521b44cbd5e292550330f9c39
lvnc9 <sam.mohammadi2154@gmail.com>
2022-11-26 12:00:22 +0330

    apply, close

--- Days/2022/Nov 22/Preferences.py
+++ Days/2022/Nov 22/Preferences.py
@@ -16,11 +16,11 @@ class Window(TkUtil.Dialog):
                 TkUtil.OK_BUTTON|TkUtil.CANCEL_BUTTON)
         
     def body(self, master):
-        self.create_varialbes()
+        self.create_variables()
         self.create_widgets(master)
         self.create_layout()
         self.create_bindings()
-        return self.frame, self.columnSpinbox
+        return self.frame, self.columnsSpinbox
 
     def create_variables(self):
         self.columns = tk.StringVar()
@@ -34,8 +34,17 @@ class Window(TkUtil.Dialog):
         self.frame = ttk.Frame(master)
         self.columnsLabel = TkUtil.Label(self.frame, text="Columns", 
                 underline=2)
+        
         self.columnsSpinbox = TkUtil.Spinbox(self.frame,
                 textvariable=self.columns, from_=Board.MIN_COLUMNS)
+        
+        self.rowsLabel = TkUtil.Label(self.frame, text="Rows")
+        self.rowsSpinbox = TkUtil.Spinbox(self.frame,
+                textvariable=self.rows, from_=Board.MIN_ROWS)
+        
+        self.maxColorsLabel = TkUtil.Label(self.frame, text="Max-Color")
+        self.maxColorsSpinbox = TkUtil.Spinbox(self.frame,
+                textvariable=self.maxColors, from_=Board.MAX_COLOR)
 
     def validate_int(self, spinboxName, number):
         return TkUtil.validate_spinbox_int(getattr(self, spinboxName),
@@ -57,4 +66,22 @@ class Window(TkUtil.Dialog):
             self.bind("<Alt-r>", lambda *args: self.rowsSpinbox.focus())
             self.bind("<Alt-m>", lambda *args: self.maxColorsSpinbox.focus())
 
+    def apply(self):
+        columns = int(self.columns.get())
+        rows = int(self.rows.get())
+        maxColors = int(self.maxColors.get())
+        newGame = (columns != self.board.Columns or
+                rows != self.board.rows or
+                maxColors != self.board.maxColors)
+        if newGame:
+            self.board.rows = columns
+            self.board.rows = rows
+            self.maxColors = maxColors
+            self.board.newGame()
+    
+    application = tk.Tk()
+
+    def close(self, event):
+        self.application.quit()
+    
 # End
