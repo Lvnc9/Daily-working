76f6ff2d43f21a88e56a18adf0b459810a733e76
lvnc9 <sam.mohammadi2154@gmail.com>
2022-12-15 01:26:53 +0330

    increae and decrease functions

--- Days/2022/Dec 13/misized-words.py
+++ Days/2022/Dec 13/misized-words.py
@@ -6,6 +6,9 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
+
+
+
 # Main class
 class StickyAandB(tk.Tk):
     """ making a moving word """
@@ -23,6 +26,27 @@ class StickyAandB(tk.Tk):
 
 class StickyColoredAandB(tk.Tk):
 
+    def __init__(self, lbl_text):
+        self.lbl_text = lbl_text
+        self.label = tk.Label(master=self, text=lbl_text)
+        self.label["text"] = "Good bye"
+        lbl_value = tk.Label(master=self, text="0")
+        lbl_value.grid(row=0 column=1)
+    
+    text = lbl_text["text"]
+    lbl_value = tk.Label(master=window, text="0")
+    lbl_value.grid(row=0, column=1)
+    # Increase decrease funcitons
+    @classmethod
+    def increase(cls):
+        value = int(cls.lbl_value["text"])
+        cls.lbl_value["text"] = f"{value + 1}"
+
+    @classmethod
+    def decrease(cls):
+        value = int(cls.lbl_value["text"])
+        cls.lbl_value["text"] = f"{value - 1}"
+
     def colored_A_B(self):
         self.columnconfigure(0, minsize=50)
         self.rowconfigure([0, 1, 2, 3], minsize=50)
@@ -37,12 +61,26 @@ class StickyColoredAandB(tk.Tk):
         lbl3.grid(row=0, column=2, sticky="ns")
         lbl4.grid(row=0, column=3, sticky="nsew")
 
+    def increase_decrease_buttons(self):
+
+        self.columnconfigure(0, minsize=50, weight=1)
+        self.rowconfigure([0, 1, 2], minsize=50, weight=1)
+
+        btn_increase = tk.Button(master=self, text="+", command=self.increase())
+        btn_increase.grid(row=0, column=0, sticky=tk.BOTH)
+
+        btn_decrease = tk.Button(master=self, text="-", command=self.decrease())
+        btn_decrease.grid(row=0, column=2, sticky=tk.BOTH)
+            
+
 
 if __name__ == "__main__":
-    hmmm = Lala()
-    hmmm.moving_A_B()
-    hmmm.mainloop()
-
+    #hmmm = StickyAandB()
+#    hmmm.moving_A_B()
+#    hmmm.mainloop()
+    hmmm2 = StickyColoredAandB()
+    hmmm2.colored_A_B()
+    hmmm2.mainloop()
 
 
 
